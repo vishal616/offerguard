f613cf76ab4a48bd17d02fd2a5f0b3477697dd9c
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-08-25 14:05:33 +0530

    initial commit

---
+++ src/main/java/com/twentyone/offerguard/OfferguardApplication.java
@@ -0,0 +1,13 @@
+package com.twentyone.offerguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferguardApplication.class, args);
+    }
+
+}
