9df2f38bc6e8565fbd7250672ce69440a5071459
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-08-25 16:06:37 +0530

    adding offer model

---
+++ src/main/java/com/twentyone/offerguard/models/Offer.java
@@ -0,0 +1,36 @@
+package com.twentyone.offerguard.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Offer {
+
+	private String id;
+	private String name;
+	private String status;
+	private String category;
+	private String currency;
+	private String price;
+	private String model;
+	private String startDate;
+	private String endDate;
+	private String previewUrl;
+	private String countryAllowed;
+	private String countryBlocked;
+	private String cityAllowed;
+	private String cityBlocked;
+	private String osAllowed;
+	private String osBlocked;
+	private String deviceAllowed;
+	private String deviceBlocked;
+	private String ispAllowed;
+	private String ispBlocked;
+	private String clickUrl;
+	private String impressionUrl;
+	private String redirects;
+
+}
