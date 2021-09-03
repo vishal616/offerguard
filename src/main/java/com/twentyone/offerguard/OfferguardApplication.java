baf9351257e206778b95fb7bc60da3eb643031ea
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-09-03 12:52:40 +0530

    hack to not put server in idle mode

--- src/main/java/com/twentyone/offerguard/OfferguardApplication.java
+++ src/main/java/com/twentyone/offerguard/OfferguardApplication.java
@@ -1,7 +1,9 @@
 package com.twentyone.offerguard;
 
+import com.twentyone.offerguard.config.Heroku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -11,6 +13,12 @@ public class OfferguardApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(OfferguardApplication.class, args);
+
+	}
+
+	@Bean
+	public Heroku heroku(){
+		return new Heroku();
 	}
 
 }
---
+++ src/main/java/com/twentyone/offerguard/config/Heroku.java
@@ -0,0 +1,20 @@
+package com.twentyone.offerguard.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+public class Heroku {
+
+	@Scheduled(fixedDelay=120000)
+	public void herokuNotIdle(){
+		log.info("Heroku not idle execution");
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			restTemplate.getForObject("http://offer-guard.herokuapp.com", Object.class);
+		} catch (Exception e) {
+			log.info("Heroku not idle execution");
+		}
+	}
+}
