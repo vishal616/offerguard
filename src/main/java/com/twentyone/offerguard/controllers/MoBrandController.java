c9c1d33a5e771afd8d2e6ec3eac38cb571371b3a
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-09-06 14:40:46 +0530

    async api logic fix

--- src/main/java/com/twentyone/offerguard/controllers/MoBrandController.java
+++ src/main/java/com/twentyone/offerguard/controllers/MoBrandController.java
@@ -1,23 +1,21 @@
 package com.twentyone.offerguard.controllers;
 
 import com.twentyone.offerguard.affiliateVendors.MoBrandVendor;
+import com.twentyone.offerguard.models.MoBrandResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/mobrand")
 @Slf4j
 public class MoBrandController {
 
-	@PostMapping("/")
-	public void postAllMoBrandAsyncResponse(@RequestBody List<Object> response) {
-		log.info("post mobrand rest api requested");
-		log.info("response length {} with response like {}", response.size(), response);
-		response.forEach((res)->{
-			log.info(res.toString());
-		});
+	@PostMapping("/{offerId}/result")
+	public void receiveMoBrandAsyncResponse(@RequestBody MoBrandResponse moBrandResponse, @PathVariable String offerId) {
+		log.info("receiving response from mobrand async rest api");
+		log.info("response like {} {} for offerid :: {}", moBrandResponse,moBrandResponse.toString(), offerId);
+		log.info("some fields {} {}", moBrandResponse.getBundleIdMatch(), moBrandResponse.getResultFlags());
 	}
 
 	@GetMapping("/job")
